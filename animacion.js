2ab756a60e90ea0bce7d2ff6751230421d418c9f
marvin0754 <rhyoarias@gmail.com>
2023-08-21 01:39:18 -0500

    Proyecto5 Animaciones con SASS v1.0

--- animacion.js
+++ animacion.js
@@ -6,6 +6,8 @@ function btn(){
     const state = { deg: 0 };
     preview(btn1,state);
     next(btn2,state);
+   
+    
 }
 
 function preview(btn1,state){
@@ -13,11 +15,17 @@ function preview(btn1,state){
     let degMin=1080;
     btn1.forEach(btn => {
         btn.firstChild.addEventListener('click',function(){
-                state.deg-=90;
-                if(state.deg>degMin){
-                    state.deg=-90;
-                }
-                cubo.style.transform=`rotateY(${state.deg}deg)`;
+            state.deg-=90;
+            if(state.deg>degMin){
+                state.deg=-90;
+            }
+            cubo.style.transform=`rotateY(${state.deg}deg)`;
+            console.log(state.deg);
+            if((state.deg+180) % 360 == 0 && state.deg != 0){
+                animacion3();
+            }else{
+                removeAnimacion3();
+            }
         })
     });
 }
@@ -31,6 +39,24 @@ function next(btn2,state){
                 state.deg=90;
             }
             cubo.style.transform=`rotateY(${state.deg}deg)`;
+            console.log(state.deg);
+            if((state.deg + 180) % 360 == 0 && state.deg != 0){
+                animacion3();
+            }else{
+                removeAnimacion3();
+            }
         });
     });
+}
+function animacion3(){
+    let texto= document.getElementsByClassName('text-1');
+    texto=Array.from(texto)[0]
+    texto.classList.add('moveScene');
+    texto.classList.add('typingEffect');
+}
+function removeAnimacion3(){
+    let texto= document.getElementsByClassName('text-1');
+    texto=Array.from(texto)[0]
+    texto.classList.remove('moveScene');
+    texto.classList.remove('typingEffect');
 }
